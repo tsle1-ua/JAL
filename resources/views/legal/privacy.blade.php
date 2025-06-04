8c13a76e524d3fe131467ab7e225dcfa4456948e
teferi laforga <99018514+tsle1-ua@users.noreply.github.com>
2025-06-04 10:45:46 +0200

    Add legal policy views

---
+++ resources/views/legal/privacy.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Política de Privacidad</h1>
+    <p>Este es un texto de ejemplo para la política de privacidad.</p>
+    <a href="{{ route('home') }}" class="btn btn-primary mt-3">Volver al inicio</a>
+</div>
+@endsection
